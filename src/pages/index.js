bf903e3e3f9e57d1721fe4f06bd4fbebfa8f7087
Josiah <Josiahsingh1996@gmail.com>
2019-08-02 00:50:56 +0300

    updated with links and createPage plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,47 @@
 import React from "react"
+import { graphql, Link } from "gatsby"
+import Header from "../components/Header"
 
-export default () => <div>Hello world!</div>
+const Layout = ({ data }) => {
+  const { edges } = data.allMarkdownRemark
+  console.log(edges)
+  return (
+    <div>
+      <Header />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          fontFamily: "avenir",
+        }}
+      >
+        {edges.map(edge => {
+          const { frontmatter } = edge.node
+          return (
+            <div key={frontmatter.title} style={{ marginBottom: "1rem" }}>
+              <Link to={frontmatter.path}>{frontmatter.title}</Link>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export const query = graphql`
+  query HomepageQuery {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          frontmatter {
+            title
+            path
+            date
+          }
+        }
+      }
+    }
+  }
+`
+export default Layout
